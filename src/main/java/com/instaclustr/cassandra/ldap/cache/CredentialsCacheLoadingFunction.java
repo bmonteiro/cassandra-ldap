1a57961bf8fb2d3b054d4c496ed16f9626417ac7
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2019-04-16 10:10:37 +1000

    changed log level in CredentialsCacheLoadingFunction to debug

--- src/main/java/com/instaclustr/cassandra/ldap/cache/CredentialsCacheLoadingFunction.java
+++ src/main/java/com/instaclustr/cassandra/ldap/cache/CredentialsCacheLoadingFunction.java
@@ -54,25 +54,23 @@ public class CredentialsCacheLoadingFunction implements Function<User, String>
     {
         try
         {
-            logger.info("Doing password authentication against " + user.getUsername());
+            logger.debug("Doing password authentication against " + user.getUsername());
 
             return getPasswordAuthLoadingFunction().apply(user);
         }
         catch (Exception ex)
         {
-            logger.info("Doing LDAP authentication against " + user.getUsername());
+            logger.debug("Doing LDAP authentication against " + user.getUsername());
 
             // we are in this catch block because
             // 1) there was not such role found in Cassandra DB hence we have to go against LDAP
             // 2) there was such role but that role does not have password in the database
             //    so we have nothing to check against so we need to do authentication against LDAP.
             //    This happens e.g. with admin user which is created but it does not have its password hashed there since it is in LDAP
-            //    This also means what we will be able to login with "cassandra:cassandra" combination with the necessity to ever reach LDAP server.
+            //    This also means what we will be able to login with "cassandra:cassandra" combination without the necessity to ever reach LDAP server.
             //
             // If there is such username and password in the database, we will never reach this catch and
             // from user perspective it acts as if there was not any LDAP server at all, we are effectively doing PasswordAuthenticator
-            //
-            // If something from 1) and 2) occurs, we have to go against LDAP
 
             return getLdapAuthLoadingFunction().apply(user);
         }
